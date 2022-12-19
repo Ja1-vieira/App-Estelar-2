ba511b899580f016cddeb55efeb268926912ff6d
Ja1-vieira <91163288+Ja1-vieira@users.noreply.github.com>
2022-12-19 10:44:21 -0300

    Add files via upload

---
+++ atividade-aula-89/App.js
@@ -0,0 +1,27 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from "./screens/Home";
+import SpaceCraftScreen from "./screens/SpaceCraft";
+import StarMapScreen from "./screens/StarMap";
+import DailyPicScreen from "./screens/DailyPic"
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="SpaceCraft" component={SpaceCraftScreen} />
+        <Stack.Screen name="StarMap" component={StarMapScreen} />
+        <Stack.Screen name="DailyPic" component={DailyPicScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
